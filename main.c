54233014f675be2473babb6da8e021227f65307c
OutOfTheBots <30621667+OutOfTheBots@users.noreply.github.com>
2019-11-09 20:50:53 +1000

    Add files via upload

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ static void MX_TIM5_Init(void);
 static void MX_TIM4_Init(void);
 
 void stepper_init();
-void set_speed(float target_speed);
+void set_speed(uint8_t motor, float target_speed);
 
 #define freq_source 80000000 //internal clock source freq
 
@@ -24,41 +24,42 @@ uint16_t prescaler = 40; //prescaler used by timer
 uint32_t freq_counter; //the freq of the timer calculated from freq_source and prescaler
 uint16_t init_speed = 5000; //this sets the acceleration by setting the timing of first step, the smaller the number the faster the acceleration
 uint16_t SPR = 3200; //steps per revolution of the stepper motor
-float tick_freq; //the freq that the steps need to be calculated from frq_counter RPM and SPR
-float speed = 0; //the current speed measured by timer ticks in ARR value to count up to
-float target_speed = 0; //the target speed that speed is accelerating towards
+float tick_freq[4]; //the freq that the steps need to be calculated from frq_counter RPM and SPR
+float speed[4]; //the current speed measured by timer ticks in ARR value to count up to
+float target_speed[4]; //the target speed that speed is accelerating towards
 
-
-int32_t n = 0;
-
-uint8_t speed_up;
-int8_t new_dir=1;
-int8_t old_dir=1;
-uint8_t second_time = 0;
-
-
-
-
+int32_t n[4];
+uint8_t speed_up[4]; //boolean used to tell whether in speed up process or slow down
+int8_t new_dir[4]; //the final direction desired
+int8_t old_dir[4]; //the direction before
+uint8_t second_time[4]; //boolean used to create double toggle of step pin i.e high/low
 
 
 int main(void){
   SystemClock_Config();
   stepper_init();
 
-  set_speed(200);
-  HAL_Delay(5000);
+  //test some speeds :)
+  set_speed(0, 5);
+  set_speed(1, 10);
+  set_speed(2, 20);
+  set_speed(3, 40);
 
-  set_speed(-200);
-  HAL_Delay(5000);
+  HAL_Delay(8000);
 
-  set_speed(-100);
-  HAL_Delay(5000);
+  set_speed(0, -100);
+  set_speed(1, -100);
+  set_speed(2, -100);
+  set_speed(3, -100);
 
-  set_speed(0);
-  HAL_Delay(5000);
+  HAL_Delay(8000);
 
-  set_speed(200);
-  HAL_Delay(5000);
+  set_speed(0, 0);
+  set_speed(1, 0);
+  set_speed(2, 0);
+  set_speed(3, 0);
+
+
 
   while (1)  {
   	  //HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
@@ -82,7 +83,7 @@ void stepper_init(){
 	  TIM3->DIER = TIM_DIER_UIE; // Enable update interrupt (timer level)
 	  TIM3->CR1 |= TIM_CR1_CEN;   // Enable timer
 	  NVIC_EnableIRQ(TIM3_IRQn); // Enable interrupt from TIM3 (NVIC level)
-	  GPIOE->ODR |= GPIO_ODR_OD1; //set dir pin high
+	  GPIOE->ODR &= ~GPIO_ODR_OD1; //set dir pin high
 
 
 
@@ -90,51 +91,57 @@ void stepper_init(){
 	  TIM4->ARR = 10;           // Auto reload value
 	  TIM4->DIER = TIM_DIER_UIE; // Enable update interrupt (timer level)
 	  TIM4->CR1 = TIM_CR1_CEN;   // Enable timer
-	  //NVIC_EnableIRQ(TIM4_IRQn); // Enable interrupt(NVIC level)
+	  NVIC_EnableIRQ(TIM4_IRQn); // Enable interrupt(NVIC level)
+	  GPIOE->ODR &= ~GPIO_ODR_OD3; //set dir pin high
 
 	  TIM5->PSC = 50;         // Set prescaler
 	  TIM5->ARR = 65535;           // Auto reload value
 	  TIM5->DIER = TIM_DIER_UIE; // Enable update interrupt (timer level)
 	  TIM5->CR1 = TIM_CR1_CEN;   // Enable timer
-	  //NVIC_EnableIRQ(TIM5_IRQn); // Enable interrupt(NVIC level)
+	  NVIC_EnableIRQ(TIM5_IRQn); // Enable interrupt(NVIC level)
+	  GPIOE->ODR &= ~GPIO_ODR_OD5; //set dir pin high
 
 	  TIM7->PSC = 50;         // Set prescaler
 	  TIM7->ARR = 1;           // Auto reload value
 	  TIM7->DIER = TIM_DIER_UIE; // Enable update interrupt (timer level)
 	  TIM7->CR1 = TIM_CR1_CEN;   // Enable timer
-	  //NVIC_EnableIRQ(TIM7_IRQn); // Enable interrupt(NVIC level)
+	  NVIC_EnableIRQ(TIM7_IRQn); // Enable interrupt(NVIC level)
+	  GPIOE->ODR &= ~GPIO_ODR_OD7; //set dir pin high
 }
 
 
-void set_speed(float RPM){
-	if(RPM > 0){
-		new_dir = 1;
+void set_speed(uint8_t motor, float RPM){
+
+	if(RPM > 0){ //set dirction boolean
+		new_dir[motor] = 1;
 	}else if (RPM < 0) {
-		new_dir = 0;
-		RPM = -1 * RPM;
+		new_dir[motor] = 0;
+		RPM = -1 * RPM; //if RPM is negative then flip sign
 	}
 
-	if(RPM != 0){
+	if(RPM != 0){ //only enters this loop if desired RPM isn't zero
 
-		tick_freq = 2 * SPR * RPM / 60;
-		target_speed = freq_counter / tick_freq;
+		tick_freq[motor]  = 2 * SPR * RPM / 60;
+		target_speed[motor]  = freq_counter / tick_freq[motor] ;
 
-		if(target_speed > 65535){
-			target_speed = 65535;
-		}
-		if((speed == 0) && (target_speed !=0)){
-			speed = init_speed;
-			n = 0;
+		if(target_speed[motor]  > 65535){ //check that the desired RPM hasn't caused the ARR to overflow
+			target_speed[motor]  = 65535;
 		}
 
-		if(target_speed < speed){
-			speed_up = 1;
+		if((speed[motor] == 0) && (target_speed[motor] !=0)){ //if stopped and want to start again
+			speed[motor] = init_speed;
+			n[motor] = 0;
+		}
+
+		if(target_speed[motor]  < speed[motor]){ //set boolean to either speed up or slow down
+			speed_up[motor] = 1;
 		}else {
-			speed_up = 0;
+			speed_up[motor] = 0;
 		}
-	}else{
-		target_speed = 0;
-		speed_up = 0;
+
+	}else{ //only does this if desired to stop
+		target_speed[motor] = 0;
+		speed_up[motor] = 0;
 	}
 }
 
@@ -143,50 +150,55 @@ void TIM3_IRQHandler(void){
 	if(TIM3->SR & TIM_SR_UIF){ // if UIF flag is set
 		TIM3->SR &= ~TIM_SR_UIF; // clear UIF flag
 
-		if((speed != 0) || (target_speed != 0)){
-			HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_0);
-			if(second_time){
+		if((speed[0] != 0) || (target_speed[0] != 0)){ //only enters this loop if not at stand still
+			HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_0); //toggle the step pin, it requires this to happen twice to complete 1 step i.e high/low
 
-				if(new_dir == old_dir){
-					if(((target_speed > init_speed - 100) || (target_speed == 0)) && (speed > init_speed - 100)){
-						speed = target_speed;
-					}else if ((speed > init_speed - 100) && (target_speed < init_speed - 100) && (speed_up) && (target_speed !=0)){
-						speed = init_speed;
-						n = 0;
+			if(second_time[0]){ //only enters this loop every second time that way the toggle happens twice to every 1 loop
+
+				if(new_dir[0] == old_dir[0]){ //only enters this loop if current direction is same as desired direction
+
+					if(((target_speed[0] > init_speed - 100) || (target_speed[0] == 0)) && (n[0] < 2)){
+						speed[0] = target_speed[0]; //if desired speed is slow and current speed is slow then goto straight to it
+					}else if ((speed[0] > init_speed - 100) && (target_speed[0] < init_speed - 100) && (speed_up[0]) && (target_speed[0] !=0)){
+						speed[0] = init_speed; //if current speed is slow and desired speed is fast then goto start acceleration speed
+						n[0] = 0;
 					}
 
-					if ((speed_up) && (speed > target_speed)){
-						n++;
-						speed = speed - ( (2 * speed) / (4 * n + 1) );
-					}else if ((!speed_up) && ((speed < target_speed) || target_speed == 0)){
-						n--;
-						speed = (speed * (4 * n + 1) / (4 * n - 1));
+					if ((speed_up[0]) && (speed[0] > target_speed[0])){ //enter this loop if speeding up
+						n[0]++;
+						speed[0] = speed[0] - ( (2 * speed[0]) / (4 * n[0] + 1) );
+					}else if ((!speed_up[0]) && ((speed[0] < target_speed[0]) || target_speed[0] == 0)){ //enter this loop if slowing down
+						n[0]--;
+						speed[0] = (speed[0] * (4 * n[0] + 1) / (4 * n[0] - 1));
 					}
-				}else{
-					if(speed == 65000){
+
+				}else{ //only enters this loop if current direction is different to desired direction
+
+					if(speed[0] == 65000){ //if speed is super slow then toggle dir pin and set speed_up boolean to speed up
 						HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_1);
-						old_dir = new_dir;
-						speed_up = 1;
-					}else if(speed > init_speed - 100){
-						speed = 65000;
+						old_dir[0] = new_dir[0];
+						speed_up[0] = 1;
 
-					}else{
-						n--;
-						speed = (speed * (4 * n + 1) / (4 * n - 1));
+					}else if(n[0] < 2){ //if speed is slower than acceleration speed then set speed to super slow
+						speed[0] = 65000;
+
+					}else{ //onyl enters this loop if going fast to just decelerate
+						n[0]--;
+						speed[0] = (speed[0] * (4 * n[0] + 1) / (4 * n[0] - 1));
 					}
 				}
 
-				if(speed > 65535){
-					speed = 65535;
+				if(speed[0] > 65535){ //check to make sure speed won't over the ARR
+					speed[0] = 65535;
 				}
 
-				if (speed != 0){
-					TIM3->ARR = (uint32_t)speed;//update ARR
+				if (speed[0] != 0){ //if speed isn't zero then update ARR
+					TIM3->ARR = (uint32_t)speed[0];//update ARR
 				}
 			}
-			second_time = !second_time;
+			second_time[0] = !second_time[0]; //flip second_time boolean
 
-		}   	//GPIOE->ODR &= ~GPIO_ODR_OD0;//set step pin low
+		}
 	}
 }
 
@@ -195,7 +207,55 @@ void TIM4_IRQHandler(void){
 if(TIM4->SR & TIM_SR_UIF){ // if UIF flag is set
   TIM4->SR &= ~TIM_SR_UIF; // clear UIF flag
 
-  HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_3);
+  if((speed[1] != 0) || (target_speed[1] != 0)){ //only enters this loop if not at stand still
+  			HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_2); //toggle the step pin, it requires this to happen twice to complet 1 step i.e high/low
+
+  			if(second_time[1]){ //only enters this loop every second time that way the toggle happens twice to every 1 loop
+
+  				if(new_dir[1] == old_dir[1]){ //only enters this loop if current direction is same as desired direction
+
+  					if(((target_speed[1] > init_speed - 100) || (target_speed[1] == 0)) && (n[1] < 2)){
+  						speed[1] = target_speed[1]; //if desired speed is slow and current speed is slow then goto striahgt to it
+  					}else if ((speed[1] > init_speed - 100) && (target_speed[1] < init_speed - 100) && (speed_up[1]) && (target_speed[1] !=0)){
+  						speed[1] = init_speed; //if current speed is slow and desired speed is fast then goto start acceleration speed
+  						n[1] = 0;
+  					}
+
+  					if ((speed_up[1]) && (speed[1] > target_speed[1])){ //enter this loop if speeding up
+  						n[1]++;
+  						speed[1] = speed[1] - ( (2 * speed[1]) / (4 * n[1] + 1) );
+  					}else if ((!speed_up[1]) && ((speed[1] < target_speed[1]) || target_speed[1] == 0)){ //enter this loop if slowing down
+  						n[1]--;
+  						speed[1] = (speed[1] * (4 * n[1] + 1) / (4 * n[1] - 1));
+  					}
+
+  				}else{ //only enters this loop if current direction is different to desired direction
+
+  					if(speed[1] == 65000){ //if speed is super slow then toggle dir pin and set speed_up boolean to speed up
+  						HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_3);
+  						old_dir[1] = new_dir[1];
+  						speed_up[1] = 1;
+
+  					}else if(n[1] < 2){ //if speed is slower than acceleration speed then set speed to super slow
+  						speed[1] = 65000;
+
+  					}else{ //onyl enters this loop if going fast to just decelerate
+  						n[1]--;
+  						speed[1] = (speed[1] * (4 * n[1] + 1) / (4 * n[1] - 1));
+  					}
+  				}
+
+  				if(speed[1] > 65535){ //check to make sure speed won't over the ARR
+  					speed[1] = 65535;
+  				}
+
+  				if (speed[1] != 0){ //if speed isn't zero then update ARR
+  					TIM4->ARR = (uint32_t)speed[1];//update ARR
+  				}
+  			}
+  			second_time[1] = !second_time[1]; //flip second_time boolean
+
+  		}
 
   }
 }
@@ -205,8 +265,55 @@ void TIM5_IRQHandler(void){
 if(TIM5->SR & TIM_SR_UIF){ // if UIF flag is set
   TIM5->SR &= ~TIM_SR_UIF; // clear UIF flag
 
-  GPIOE->ODR |= GPIO_ODR_OD5;
-  GPIOE->ODR &= ~GPIO_ODR_OD5;
+  if((speed[2] != 0) || (target_speed[2] != 0)){ //only enters this loop if not at stand still
+  			HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_4); //toggle the step pin, it requires this to happen twice to complet 1 step i.e high/low
+
+  			if(second_time[2]){ //only enters this loop every second time that way the toggle happens twice to every 1 loop
+
+  				if(new_dir[2] == old_dir[2]){ //only enters this loop if current direction is same as desired direction
+
+  					if(((target_speed[2] > init_speed - 100) || (target_speed[2] == 0)) && (n[2] < 2)){
+  						speed[2] = target_speed[2]; //if desired speed is slow and current speed is slow then goto straight to it
+  					}else if ((speed[2] > init_speed - 100) && (target_speed[2] < init_speed - 100) && (speed_up[2]) && (target_speed[2] !=0)){
+  						speed[2] = init_speed; //if current speed is slow and desired speed is fast then goto start acceleration speed
+  						n[2] = 0;
+  					}
+
+  					if ((speed_up[2]) && (speed[2] > target_speed[2])){ //enter this loop if speeding up
+  						n[2]++;
+  						speed[2] = speed[2] - ( (2 * speed[2]) / (4 * n[2] + 1) );
+  					}else if ((!speed_up[2]) && ((speed[2] < target_speed[2]) || target_speed[2] == 0)){ //enter this loop if slowing down
+  						n[2]--;
+  						speed[2] = (speed[2] * (4 * n[2] + 1) / (4 * n[2] - 1));
+  					}
+
+  				}else{ //only enters this loop if current direction is different to desired direction
+
+  					if(speed[2] == 65000){ //if speed is super slow then toggle dir pin and set speed_up boolean to speed up
+  						HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_5);
+  						old_dir[2] = new_dir[2];
+  						speed_up[2] = 1;
+
+  					}else if(n[2] < 2){ //if speed is slower than acceleration speed then set speed to super slow
+  						speed[2] = 65000;
+
+  					}else{ //onyl enters this loop if going fast to just decelerate
+  						n[2]--;
+  						speed[2] = (speed[2] * (4 * n[2] + 1) / (4 * n[2] - 1));
+  					}
+  				}
+
+  				if(speed[2] > 65535){ //check to make sure speed won't over the ARR
+  					speed[2] = 65535;
+  				}
+
+  				if (speed[2] != 0){ //if speed isn't zero then update ARR
+  					TIM5->ARR = (uint32_t)speed[2];//update ARR
+  				}
+  			}
+  			second_time[2] = !second_time[2]; //flip second_time boolean
+
+  		}
 
   }
 }
@@ -216,9 +323,55 @@ void TIM7_IRQHandler(void){
 if(TIM7->SR & TIM_SR_UIF){ // if UIF flag is set
   TIM7->SR &= ~TIM_SR_UIF; // clear UIF flag
 
-  GPIOE->ODR |= GPIO_ODR_OD7;
-  GPIOE->ODR &= ~GPIO_ODR_OD7;
+  if((speed[3] != 0) || (target_speed[3] != 0)){ //only enters this loop if not at stand still
+  			HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_6); //toggle the step pin, it requires this to happen twice to complet 1 step i.e high/low
 
+  			if(second_time[3]){ //only enters this loop every second time that way the toggle happens twice to every 1 loop
+
+  				if(new_dir[3] == old_dir[3]){ //only enters this loop if current direction is same as desired direction
+
+  					if(((target_speed[3] > init_speed - 100) || (target_speed[3] == 0)) && (n[3] < 2)){
+  						speed[3] = target_speed[3]; //if desired speed is slow and current speed is slow then goto striahgt to it
+  					}else if ((speed[3] > init_speed - 100) && (target_speed[3] < init_speed - 100) && (speed_up[3]) && (target_speed[3] !=0)){
+  						speed[3] = init_speed; //if current speed is slow and desired speed is fast then goto start acceleration speed
+  						n[3] = 0;
+  					}
+
+  					if ((speed_up[3]) && (speed[3] > target_speed[3])){ //enter this loop if speeding up
+  						n[3]++;
+  						speed[3] = speed[3] - ( (2 * speed[3]) / (4 * n[3] + 1) );
+  					}else if ((!speed_up[3]) && ((speed[3] < target_speed[3]) || target_speed[3] == 0)){ //enter this loop if slowing down
+  						n[3]--;
+  						speed[3] = (speed[3] * (4 * n[3] + 1) / (4 * n[3] - 1));
+  					}
+
+  				}else{ //only enters this loop if current direction is different to desired direction
+
+  					if(speed[3] == 65000){ //if speed is super slow then toggle dir pin and set speed_up boolean to speed up
+  						HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_7);
+  						old_dir[3] = new_dir[3];
+  						speed_up[3] = 1;
+
+  					}else if(n[3] < 2){ //if speed is slower than acceleration speed then set speed to super slow
+  						speed[3] = 65000;
+
+  					}else{ //onyl enters this loop if going fast to just decelerate
+  						n[3]--;
+  						speed[3] = (speed[3] * (4 * n[3] + 1) / (4 * n[3] - 1));
+  					}
+  				}
+
+  				if(speed[3] > 65535){ //check to make sure speed won't over the ARR
+  					speed[3] = 65535;
+  				}
+
+  				if (speed[3] != 0){ //if speed isn't zero then update ARR
+  					TIM7->ARR = (uint32_t)speed[3];//update ARR
+  				}
+  			}
+  			second_time[3] = !second_time[3]; //flip second_time boolean
+
+  		}
   }
 }
 
